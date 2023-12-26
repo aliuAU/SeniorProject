b445698899cc4480888039d6338d4dc4d1db5985
Ali Uğurlu <100438322+aliuAU@users.noreply.github.com>
2023-12-26 20:47:14 +0300

    Add files via upload

---
+++ Migrations/20231218150447_ratingimplement.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmFolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class ratingimplement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AverageRating",
+                table: "Movies",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageRating",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Comments");
+        }
+    }
+}
